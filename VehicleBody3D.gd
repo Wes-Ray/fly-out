8f49df9c897706f536c02fe91e7b95c98773b246
Brian Guerrero <BGuerrero825@gmail.com>
2024-07-12 23:50:10 -0500

    initial level with flat ground and a track; and terrible physics using a vehicle body

---
+++ VehicleBody3D.gd
@@ -0,0 +1,14 @@
+extends VehicleBody3D
+
+@export var MAX_STEER = 0.9
+@export var ENGINE_POWER := 600
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 10)
+	engine_force = Input.get_axis("brake", "gas") * ENGINE_POWER
