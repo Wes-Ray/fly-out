2a602779ab8c3f7579b252113f14d9f2856a2660
Brian Guerrero <BGuerrero825@gmail.com>
2024-07-13 19:40:49 -0500

    ripped wipeout car asset; added texture to track; added car model to physics body

--- VehicleBody3D.gd
+++ VehicleBody3D.gd
@@ -1,7 +1,8 @@
 extends VehicleBody3D
 
-@export var MAX_STEER = 0.9
-@export var ENGINE_POWER := 600
+@export var MAX_STEER := 1.0
+@export var STEER_FORCE := 18
+@export var ENGINE_POWER := 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -10,5 +11,5 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta):
-	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * 10)
+	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * STEER_FORCE)
 	engine_force = Input.get_axis("brake", "gas") * ENGINE_POWER
