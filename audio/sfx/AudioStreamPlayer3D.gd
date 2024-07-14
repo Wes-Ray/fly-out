22d0f4689affbb16b9d9ac43739c148ca340037e
DallasHuff <dallashuff45@gmail.com>
2024-07-14 17:27:14 -0500

    created music and sfx area, filled it with engine noises and music

---
+++ audio/sfx/AudioStreamPlayer3D.gd
@@ -0,0 +1,29 @@
+extends AudioStreamPlayer3D
+
+
+@onready var h_slider : HSlider = %HSlider
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+
+	var val = h_slider.value / 50
+
+	pitch_scale = val
+
+
+
+
+func _on_button_accel_pressed() -> void:
+	playing = !playing
+
+
+func _on_button_idle_pressed() -> void:
+	playing = !playing
+
+func _on_button_rocket_pressed() -> void:
+	playing = !playing
