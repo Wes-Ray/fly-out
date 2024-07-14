1c056bc4341d9408a05ee7282851ad6f7e8136d8
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2024-07-14 08:47:24 -0500

    added basic flying controls

--- Ship/Ship.gd
+++ Ship/Ship.gd
@@ -6,33 +6,38 @@ extends CharacterBody3D
 # # Roll: left and right arrow
 # # Pitch: up and down down arrow
 
-@export var speed : float = 100.0
-@export var yaw_speed : float = 0.08
+@export var speed : float = 100
+@export var yaw_speed : float = 4
+@export var pitch_speed : float = 4
+@export var roll_speed : float = 4
 
 var throttle : float  # current throttle input
-var move_vel : Vector3  # tracks moving velocity
 var move_speed : float  # tracks current speed in the direction the ship is looking
+var rotate_input : Vector3  # tracks current input for rotation (pitch, roll, yaw)
 
-func _physics_process(delta) -> void:
+func _physics_process(delta : float) -> void:
 
 	# TODO: move to global/menu
 	if Input.is_action_just_released("ui_cancel"): get_tree().quit()
 
-	velocity = move_ship(delta)
-	move_and_slide()
+	move_ship(delta)
 
 
-func move_ship(delta) -> Vector3:
-	move_vel = Vector3.ZERO
+func move_ship(delta : float) -> void:
 
-	var yaw_input : float = Input.get_axis("yaw_right", "yaw_left")
-	rotation.y += yaw_input * yaw_speed
-
+	# Yaw
+	rotate_input.y = Input.get_axis("yaw_right", "yaw_left")	
+	# Pitch
+	rotate_input.x = Input.get_axis("pitch_down", "pitch_up")
+	# Roll
+	rotate_input.z = Input.get_axis("roll_right", "roll_left")
+	
+	# Translate forward/back
 	throttle = Input.get_axis("brake", "gas")
-	# move_vel.x += throttle * speed
-	var translate_dir : Vector3 = Vector3.ZERO
-	translate_dir.x = throttle * speed
-	move_vel = translate_dir.rotated(Vector3.UP, rotation.y + TAU/4)
 
+	rotate(basis.z, rotate_input.z * roll_speed * delta)
+	rotate(basis.x, rotate_input.x * pitch_speed * delta)
+	rotate(basis.y, rotate_input.y * yaw_speed * delta)
 
-	return move_vel
+	velocity = -basis.z * throttle * speed
+	move_and_slide()
