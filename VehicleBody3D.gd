0143a4379686563fed2cb1a6146b8d0dcbc5fbfb
Brian Guerrero <BGuerrero825@gmail.com>
2024-07-14 14:55:52 -0500

    post merge fixes / adds

--- VehicleBody3D.gd
+++ VehicleBody3D.gd
@@ -5,11 +5,11 @@ extends VehicleBody3D
 @export var ENGINE_POWER := 1000
 
 # Called when the node enters the scene tree for the first time.
-func _ready():
+func _ready() -> void:
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta):
+func _physics_process(delta: float) -> void:
 	steering = move_toward(steering, Input.get_axis("right", "left") * MAX_STEER, delta * STEER_FORCE)
 	engine_force = Input.get_axis("brake", "gas") * ENGINE_POWER
