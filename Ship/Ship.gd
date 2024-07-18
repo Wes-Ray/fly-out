2016b21ccab370d8005fe6bbdbdbb9f95c9fe9fa
Wes-Ray <42216488+Wes-Ray@users.noreply.github.com>
2024-07-17 21:25:40 -0500

    basic flying and ground physics implemented

--- Ship/Ship.gd
+++ Ship/Ship.gd
@@ -7,6 +7,7 @@ extends CharacterBody3D
 # Pitch: Right Joystick or up and down down arrow
 
 @onready var cast_center: = $cast_center
+@onready var cast_ground_detector: = $cast_ground_detector
 @onready var cast_front: = $cast_front
 @onready var cast_rear: = $cast_rear
 @onready var cast_left: = $cast_left
@@ -30,7 +31,8 @@ extends CharacterBody3D
 @export var ground_yaw_speed: float = 2
 @export var ground_friction: float = 5000
 @export var ground_max_speed: float = 150
-@export var ground_gravity: float = 4
+@export var ground_gravity: float = 20
+@export var ground_hover_target_height: float = 1
 
 var throttle: float  # current throttle input
 var current_speed: float  # tracks current acceleration in the direction the ship is looking
@@ -42,10 +44,7 @@ var velocity_vertical_component: float  # tracks vertical component for gravity 
 
 # TODO: might want to return distance_squared_to()
 func raycast_distance(raycast: RayCast3D) -> float:
-	if not raycast.is_colliding():
-		# TODO: return error value or don't allow value to be set
-		# could also default to max length? 
-		return 404
+	assert(raycast.is_colliding(), "getting raycast distance when there is no collision")
 	return global_transform.origin.distance_to(raycast.get_collision_point())
 
 
@@ -55,7 +54,8 @@ func move_ship_grounded(delta: float) -> void:
 	# TODO: rotate/steer/pitch?
 	rotate(basis.y.normalized(), rotate_input.y * air_yaw_speed * delta)
 	# TODO: update for ground specific pitch
-	# rotate(basis.x.normalized(), rotate_input.x * air_pitch_speed * 0.5 * delta)
+	HUD.debug("pitch", rotate_input.x)
+	rotate(basis.x.normalized(), rotate_input.x * air_pitch_speed * 0.5 * delta)
 
 
 	HUD.debug("throttle", throttle)
@@ -77,20 +77,42 @@ func move_ship_grounded(delta: float) -> void:
 	# should NOT be sending it to rotate calc
 	# TODO: more gradually rotate with lerp or similar effect
 
-	# handle auto-pitch to surface
-	HUD.debug("cast_front", raycast_distance(cast_front))
-	HUD.debug("cast_rear", raycast_distance(cast_rear))
-	var front_rear_diff: = raycast_distance(cast_front) - raycast_distance(cast_rear)
-	HUD.debug("front_rear_diff", front_rear_diff)
-	rotate(basis.x.normalized(), -front_rear_diff * delta)
+	# don't auto-adjust to surface if raycast is not colliding
+	if ( cast_front.is_colliding() and cast_rear.is_colliding() and 
+		 cast_left.is_colliding() and cast_right.is_colliding() ):
 
-	# handle auto-roll to surface
-	var left_right_diff: = raycast_distance(cast_left) - raycast_distance(cast_right)
-	HUD.debug("left_right_diff", left_right_diff)
-	rotate(basis.z.normalized(), left_right_diff * delta)
+		# handle auto-pitch to surface
+		var front_rear_diff: = raycast_distance(cast_front) - raycast_distance(cast_rear)
+		HUD.debug("front_rear_diff", front_rear_diff)
+		rotate(basis.x.normalized(), -front_rear_diff * delta)
+
+		# handle auto-roll to surface
+		var left_right_diff: = raycast_distance(cast_left) - raycast_distance(cast_right)
+		HUD.debug("left_right_diff", left_right_diff)
+		rotate(basis.z.normalized(), left_right_diff * delta)
 	
-	# handle ground effect sucking down to ground
-	HUD.debug("cast_center", raycast_distance(cast_center))
+	# suck to ground, max height based on length of raycast length
+	HUD.debug("ground_detect", cast_ground_detector.is_colliding())
+	if cast_center.is_colliding():
+		velocity_vertical_component += -ground_gravity * delta #* raycast_distance(cast_center)
+		velocity_vertical_component = max(-10, velocity_vertical_component)
+
+	# elif cast_ground_detector.is_colliding():
+		# velocity_vertical_component += ground_gravity * delta
+	# elif:
+	# 	velocity_vertical_component = -air_gravity
+	# 	lerp()
+		# handle ground effect sucking down to ground
+		# HUD.debug("cast_center", raycast_distance(cast_center))
+		# # TODO: add vertical component
+		# var hover_height:float = raycast_distance(cast_center)
+		# var distance_to_hover_height:float = abs(ground_hover_target_height - hover_height)
+		# # TODO: might want to make these exponential/related to hover height directly (more distance, higher grav change)
+		# if hover_height > 1.6:
+		# 	velocity_vertical_component -= ground_gravity * (distance_to_hover_height**2) * delta
+		# elif hover_height < 1.0:
+		# 	velocity_vertical_component += ground_gravity * (distance_to_hover_height**2) * delta
+
 
 	# debug movement
 	# velocity.y += rotate_input.x * delta * 10
@@ -98,7 +120,18 @@ func move_ship_grounded(delta: float) -> void:
 
 	# TODO: handle colliding with walls, likely have to reset current_speed to velocity.length()
 
+	if Input.is_action_pressed("debug1"):
+		velocity_vertical_component -= ground_gravity * delta
+	if Input.is_action_pressed("debug2"):
+		velocity_vertical_component += ground_gravity * delta
+	
+	HUD.debug("vert_component", velocity_vertical_component)
+
 	velocity = forward * current_speed
+	# TODO: add constant for this
+	# TODO: this should potentially be based off of angle compared to track? input prob works for now
+	if not rotate_input.x > 0.08:
+		velocity.y = velocity_vertical_component
 	move_and_slide()
 
 
@@ -111,7 +144,6 @@ func move_ship_flying(delta: float) -> void:
 	# a stall mechanic
 	#
 
-	HUD.debug("cast_center", raycast_distance(cast_center))
 	var forward: = -basis.z
 	var horizon: = Vector3(forward.x, 0, forward.z).normalized()
 
@@ -163,10 +195,12 @@ func move_ship_flying(delta: float) -> void:
 
 
 func is_grounded() -> bool:
-	var grounded:= true
-	HUD.debug("grounded", grounded)
+	var grounded:bool = cast_ground_detector.is_colliding()
 	if grounded: is_stalling = false  # always disable stalling when grounded
+	HUD.debug("grounded", grounded)
 	return grounded
+	# return true
+	# return false
 
 
 func move_ship(delta: float) -> void:
