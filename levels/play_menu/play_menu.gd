d96a37f0620fc25a8a8380805da7cf2927c306dd
Brian Guerrero <BGuerrero825@gmail.com>
2024-07-21 14:53:56 -0500

    relinked levels from menu

--- levels/play_menu/play_menu.gd
+++ levels/play_menu/play_menu.gd
@@ -1,12 +1,12 @@
 extends Control
 
 
-@export var tutorial_level : String = "res://levels/test_level.tscn"
-@export var play_level : String = "res://levels/basic_track_level.tscn"
+@export var advanced_level : String = "res://levels/advanced_track.tscn"
+@export var basic_level : String = "res://levels/basic_track_level.tscn"
 
 func _on_play_pressed() -> void:
 	print("pressed")
-	get_tree().change_scene_to_file(play_level)
+	get_tree().change_scene_to_file(advanced_level)
 
 func _on_tutorial_pressed() -> void:
-	get_tree().change_scene_to_file(tutorial_level)
+	get_tree().change_scene_to_file(basic_level)
